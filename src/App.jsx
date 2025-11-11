bd449e41826c5f56f2c5e29b303fdac4aa2eb73a
Saroj Adhikari <sarojadh1111@gmail.com>
2025-11-11 08:29:06 +0545

    adding incrementDecrement

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import UserCard from './props/UserCard'
 import UserList from './props/UserList'
 import data from './Data/Data.json'
 import StudentProps from './props/studentProps'
+import IncrementDecrement from './State/IncrementDecrement'
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
     { <UserCard user={data.user?.[0]} /> }
      <UserList />
      <StudentProps name="Saroj Adhikari" age={22} isStudent={true} />
+     <IncrementDecrement />
     </>
   )
 }
---
+++ src/State/IncrementDecrement.jsx
@@ -0,0 +1,18 @@
+
+import { useState } from 'react'
+const IncrementDecrement = () => {
+            const [count, setcount] = useState(0)
+  return (
+    <div className=" flex flex-col items-center bg-gray-200 min-h-screen">
+        <h2 className="text-2xl font-bold mb-6">UseState Counter Component</h2>
+        <h3 className="text-xl font-semibold mb-4">Count: {count}</h3>
+        <div className="flex space-x-4">
+        <button onClick = {()=>setcount(count+1)}className="bg-blue-500 px-3 py-1 text-white rounded ">Increment</button>
+        <button onClick = {()=>setcount(0)}className="bg-blue-500 px-3 py-1 text-white rounded">Reset</button>
+        <button onClick = {()=>setcount(count-1)}className="bg-blue-500 px-3 py-1 text-white rounded ">Decrement</button>
+        </div>
+    </div>
+  )
+}
+
+export default IncrementDecrement;
