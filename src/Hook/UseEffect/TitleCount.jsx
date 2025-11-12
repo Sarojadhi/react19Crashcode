e42634ff6ef0053f4453629be678c8ff622f58b4
Saroj Adhikari <sarojadh1111@gmail.com>
2025-11-12 19:20:08 +0545

    Title Count

---
+++ src/Hook/UseEffect/TitleCount.jsx
@@ -0,0 +1,24 @@
+import React, { useState, useEffect } from "react";
+
+const TitleCounter = () => {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    document.title = `Count: ${count}`;
+  }, [count]);
+
+  return (
+    <div className="flex flex-col items-center gap-4 mt-10">
+      <p>Check the browser tab title!</p>
+      <button
+        onClick={() => setCount(count + 1)}
+        className="bg-blue-500 text-white px-4 py-2 rounded"
+      >
+        Increase
+      </button>
+      <p>Current Count: {count}</p>
+    </div>
+  );
+};
+
+export default TitleCounter;
